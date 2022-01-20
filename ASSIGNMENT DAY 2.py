6d80b765b5b23cd948a97bfe7995533d71c38c73
Gurunanthan2003 <97939682+Gurunanthan2003@users.noreply.github.com>
2022-01-20 19:10:53 +0530

    Add files via upload

---
+++ ASSIGNMENT DAY 2.py
@@ -0,0 +1,85 @@
+
+print("guru")
+guru
+guru
+name='guru'
+name
+name
+'guru'
+age=18
+age
+18
+country=india
+ountry='india'
+country
+'india'
+# f string
+print(f'')
+
+print(f'my name is',name)
+
+print('my name is', name)
+my name is guru
+print({2+2})
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print(2+2)
+      
+4
+print(2-6)
+      
+-4
+print(78/34)
+      
+2.2941176470588234
+print(78%56)
+      
+22
+'''
+'''
+      
+'\n'
+#list
+      
+total_items=list()
+      
+total_items.append(guru)
+      
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    total_items.append(guru)
+NameError: name 'guru' is not defined
+total_items.append(55)
+      
+total_items,append("guru")
+      
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    total_items,append("guru")
+NameError: name 'append' is not defined
+total_items.append("guru")
+      
+total_items.append(84.5)
+      
+total_items
+      
+[55, 'guru', 84.5]
+min([8,4,2,5,4])
+      
+2
+max([85,90,70,60,55])
+      
+90
+sum([10,20,30])
+      
+60
+
+# tuple
+
+tuple_a=tupe()
+type(tuple_a)
+
+
+\
+  
+      
